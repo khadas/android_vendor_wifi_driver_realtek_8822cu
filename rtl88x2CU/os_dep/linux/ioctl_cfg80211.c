27675d6985d5fcc3a802d37362d93dbb4ddac134
libo <bo.li@amlogic.com>
2021-04-16 23:50:41 +0800

    wifi: modify for amlogic platform [2/2]
    PD#SWPL-47393
    
    Problem:
    need add 8822cu support for kernel 5.4
    
    Solution:
    add 8822cu support for kernel 5.4
    
    Verify:
    T3
    
    Signed-off-by: libo <bo.li@amlogic.com>
    Change-Id: I6e339b7828032874a62004b81aa2e3a47a23e28f

--- rtl88x2CU/os_dep/linux/ioctl_cfg80211.c
+++ rtl88x2CU/os_dep/linux/ioctl_cfg80211.c
@@ -10523,7 +10523,8 @@ int rtw_wiphy_register(struct wiphy *wiphy)
 {
 	RTW_INFO(FUNC_WIPHY_FMT"\n", FUNC_WIPHY_ARG(wiphy));
 
-#if ( (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 14, 0)) \
+#if ( ((LINUX_VERSION_CODE < KERNEL_VERSION(5, 3, 0)) \
+     && (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 14, 0))) \
      || defined(RTW_VENDOR_EXT_SUPPORT) )
 	rtw_cfgvendor_attach(wiphy);
 #endif
@@ -10537,7 +10538,8 @@ void rtw_wiphy_unregister(struct wiphy *wiphy)
 {
 	RTW_INFO(FUNC_WIPHY_FMT"\n", FUNC_WIPHY_ARG(wiphy));
 
-#if ( (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 14, 0)) \
+#if ( ((LINUX_VERSION_CODE < KERNEL_VERSION(5, 3, 0)) \
+     && (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 14, 0))) \
      || defined(RTW_VENDOR_EXT_SUPPORT) )
 	rtw_cfgvendor_detach(wiphy);
 #endif
